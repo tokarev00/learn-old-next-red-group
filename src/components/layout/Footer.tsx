19400a1150fdb2baf682b1b573195af7f605aed1
tokarev00 <65186416+tokarev00@users.noreply.github.com>
2023-05-24 22:21:22 +0300

    try to disable ssr to component Footer

---
+++ src/components/layout/Footer.tsx
@@ -0,0 +1,6 @@
+import {FC} from 'react';
+
+const Footer: FC = () => {
+    return <footer>Footer</footer>
+}
+export default Footer
--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,11 +1,17 @@
 import {FC, PropsWithChildren} from "react";
 import Header from '../layout/header/Header'
 import Meta from "../seo/Meta";
+import Footer from "./Footer";
+import dynamic, {noSSR} from "next/dynamic";
+const DynamicFooter = dynamic(() => import('./Footer'), {
+    ssr: false
+});
 const Layout: FC<PropsWithChildren<IMeta>> = ({children, title, description}) => {
     return (
         <Meta title={title} description={description} >
             <Header/>
             {children}
+            <DynamicFooter />
         </Meta>)
 }
 export default Layout;
